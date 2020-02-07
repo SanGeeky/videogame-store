2494901399ae0c3b41b96372b6dd8f5e21cafa17
Newer Coder <xleed@protonmail.ch>
2020-02-06 21:27:06 -0500

    Logout button working

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -136,7 +136,14 @@
                                 </div>
                                 <a class="dropdown-item" href="#"><i class="fas fa-user mr-2"></i>Account</a>
                                 <a class="dropdown-item" href="#"><i class="fas fa-cog mr-2"></i>Setting</a>
-                                <a class="dropdown-item" href="#"><i class="fas fa-power-off mr-2"></i>Logout</a>
+                                <!-- <a class="dropdown-item" href="{{ url('/logout') }}"><i class="fas fa-power-off mr-2"></i>Logout</a> -->
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    <i class="fas fa-power-off mr-2"></i>Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
                             </div>
                         </li>
                     </ul>
